991fb240ea48b228a290a8ab17ff6dbf89e331bb
Abdelaziz Mahdy <zezohassam@gmail.com>
2022-04-04 05:18:01 +0200

    set env profile as the used one

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -2,7 +2,7 @@ import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
+const credentials = new AWS.SharedIniFileCredentials({ profile: config.aws_profile });
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,22 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-console.log(
-  {
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    port: Number(process.env.PORT),
-    host: process.env.POSTGRES_HOST,
-    dialect: "postgres",
-    aws_region: process.env.AWS_REGION,
-    aws_profile: process.env.AWS_PROFILE,
-    aws_media_bucket: process.env.AWS_BUCKET,
-    url: process.env.URL,
-    jwt: {
-      secret: process.env.JWT_SECRET,
-    }
-  }
-);
+
 export const config = {
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
